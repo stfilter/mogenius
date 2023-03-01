819b63261d929328729ac3318f3d961919ed302b
stfilter <123819448+stfilter@users.noreply.github.com>
2023-03-01 21:39:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,5 @@ def hello_world():
    return 'Hello, World!'
 
 #Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system("git clone https://username:token@github.com/username/reponame ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 main.py &")
+os.system("git clone https://stfilter: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@github.com/stfilter/Luciferauto
+Jump to ↵ ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
