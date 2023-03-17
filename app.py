b5c0fe6ca8b908ccc78ca46fc240fbdbb0ef158f
stfilter <123819448+stfilter@users.noreply.github.com>
2023-03-17 22:31:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ def hello_world():
    return 'Hello, World!'
 
 #Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system("git clone https://stfilter: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@github.com/stfilter/Luciferauto
+os.system("git clone https://stfilter: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@github.com/https://github.com/stfilter/Stleech
 Jump to ↵ ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
